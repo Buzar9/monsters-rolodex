af031c8cc3ebe47e3d0846c6f82e805555561268
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-18 12:33:04 +0200

    Updates

---
+++ precache-manifest.c53759fc64d4d945012f41e2b6dd596c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b57901a0f3aa0ef30902a3fbe026283",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "990b03aa762e85c76e20",
+    "url": "/monsters-rolodex/static/css/main.e24604fe.chunk.css"
+  },
+  {
+    "revision": "b72796cf3f3f640f1c22",
+    "url": "/monsters-rolodex/static/js/2.0f979dc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0f979dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990b03aa762e85c76e20",
+    "url": "/monsters-rolodex/static/js/main.6ebe1b98.chunk.js"
+  },
+  {
+    "revision": "92e14a2772d670da2803",
+    "url": "/monsters-rolodex/static/js/runtime-main.d6897594.js"
+  }
+]);
